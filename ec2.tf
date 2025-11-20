36f65b17faa819be6d94a498e07364c6836180cf
Unais Rashid <unais@Unaiss-MacBook-Pro.local>
2025-11-20 17:57:32 +0000

    terraform create

---
+++ ec2.tf
@@ -0,0 +1,8 @@
+resource "aws_instance" "this" {
+    ami = ami-0f50f13aefb6c0a5d
+    instance_type = t3.micro
+    tags = {
+        Name = "terraform create"
+    }
+    user_data_replace_on_change = false
+}
